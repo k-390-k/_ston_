76331dd3396b864225e58812ab03c88ea110d07d
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-22 13:59:34 +0900

    記事投稿画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'recipes/index'
-  get 'recipes/show'
-  get 'recipes/new'
-  get 'recipes/edit'
   root 'toppages#index'
+
+  resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
