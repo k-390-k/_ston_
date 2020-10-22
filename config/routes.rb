cf5f84d51ce1de83e1db20233d031a130d1af976
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-22 13:54:45 +0900

    モデルとコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'recipes/index'
+  get 'recipes/show'
+  get 'recipes/new'
+  get 'recipes/edit'
   root 'toppages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
