847926e9047efabaf5414ad094aea888e25e4740
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-22 14:02:31 +0900

    createアクションの実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,6 +9,18 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    end
+  end
+
   def edit
+  end
+
+  private 
+  def recipe_params
+    params.require(:recipe).permit(:name,:description)
   end
 end
