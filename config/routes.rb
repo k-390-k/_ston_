debe76ff512909b301b59b61b3b09cd78a0abe0f
keisuke-sakuma <sakusuke0412@gmail.com>
2020-10-22 14:40:59 +0900

    コントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'toppages#index'
 
   resources :recipes
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
